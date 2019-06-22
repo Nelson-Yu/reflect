d140db5e15d8732c58c91adaa1e0f99a36ed5f0a
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-22 00:07:09 -0700

    Created sleeps migration

---
+++ express-back-end/db/migrations/20190622070632_create_sleeps.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('sleeps', table => {
+      table.increments('id');
+      table.ingeger('user_id').references('users.id')
+      table.date('date');
+      table.integer('time_sec');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('sleeps')
+  ])
+};
