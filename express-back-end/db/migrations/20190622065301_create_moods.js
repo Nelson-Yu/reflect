6c391c5c435014ba3c9dbba836e9a9c1bd7dc421
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-21 23:56:30 -0700

    Created moods migration

---
+++ express-back-end/db/migrations/20190622065301_create_moods.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('moods', table => {
+      table.increments('id');
+      table.ingeger('user_id').references('users.id')
+      table.date('date');
+      table.integer('rank');
+      table.integer('emoji_rank');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('moods')
+  ])
+};
