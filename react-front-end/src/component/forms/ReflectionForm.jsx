389cf983eac1be30b15e9f20248474d9ed617a92
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-24 15:24:46 -0700

    Added reflection form with textareas

---
+++ react-front-end/src/component/forms/ReflectionForm.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import { Input, Form, Button } from 'antd';
+
+const { TextArea } = Input
+
+class Answer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formLayout: 'horizontal',
+    };
+  }
+
+  // fetchData = () => {
+  //   axios
+  //     .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+  //     .then(response => {
+  //       // handle success
+  //       console.log(response.data); // The entire response from the Rails API
+
+  //       console.log(response.data.message); // Just the message
+  //       this.setState({
+  //         message: response.data.message
+  //       });
+  //     });
+  // };
+
+  render() {
+    const { formLayout } = this.state;
+    const formItemLayout =
+      formLayout === 'vertical'
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+          }
+        : null;
+    const buttonItemLayout =
+      formLayout === 'vertical'
+        ? {
+            wrapperCol: { span: 14, offset: 4 },
+          }
+        : null;
+    return (
+      <>
+        <Form layout={formLayout}>
+          <Form.Item label="Field A" {...formItemLayout}>
+            <TextArea 
+            placeholder="input placeholder" 
+            autosize={{ minRows: 2, maxRows: 4 }}/>
+          </Form.Item>
+          <Form.Item label="Field B" {...formItemLayout}>
+            <TextArea 
+            placeholder="input placeholder" 
+            autosize={{ minRows: 2, maxRows: 4 }}/>
+          </Form.Item>
+          <Form.Item label="Field C" {...formItemLayout}>
+            <TextArea 
+            placeholder="input placeholder" 
+            autosize={{ minRows: 2, maxRows: 4 }}/>
+          </Form.Item>
+          <Form.Item {...buttonItemLayout}>
+            <Button type="primary">Submit</Button>
+          </Form.Item>
+        </Form>
+      </>
+    
+    );
+  }
+}
+
+export default Answer;
