c8772aaedaa13452017307814fd6671610e15a95
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-26 22:26:59 -0700

    Edited Styling of fitness form

--- react-front-end/src/component/forms/Checkbox.jsx
+++ react-front-end/src/component/forms/Checkbox.jsx
@@ -4,7 +4,9 @@ export const Checkbox = props => {
   return (
     <li className="activity">
       <input type="checkbox" id={props.id} className="activity-input" value={props.value} checked={props.isChecked} onClick={props.handleCheck}/>
-      <label className="activity-icon" for={props.id}><img src={props.image}/></label>
+      <label className="activity-icon" for={props.id}>
+        <img src={props.image}/> 
+      </label>
     </li>
   )
 }
