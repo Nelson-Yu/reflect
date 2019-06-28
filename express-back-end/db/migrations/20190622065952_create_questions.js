0e180cb0627f69e387947d001b9afdcb70bc6222
Liam Hetherington <liambhetherington@gmail.com>
2019-06-28 00:04:09 +0000

    ran seed

---
+++ express-back-end/db/migrations/20190622065952_create_questions.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('questions', table => {
+            table.increments('id');
+            table.string('question');
+        })
+    ])
+};
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('questions')
+    ])
+};
