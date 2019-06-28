277bc93bb92b7645e0dd1912283cbbf60b6001f2
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-27 17:12:28 -0700

    Fixed syntax error

--- react-front-end/src/component/Tasks.jsx
+++ react-front-end/src/component/Tasks.jsx
@@ -64,7 +64,7 @@ class Tasks extends Component {
 
   addToDo = title => {
     axios.post('https://jsonplaceholder.typicode.com/todos', {
-      user_id: 1
+      user_id: 1,
       title: title,
       completed: false
     })
