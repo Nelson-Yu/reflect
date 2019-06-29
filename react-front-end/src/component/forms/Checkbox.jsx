4721af255a95bb29dcf5c73aed57e890b0319005
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-29 01:08:14 -0700

    Added More Activities

--- react-front-end/src/component/forms/Checkbox.jsx
+++ react-front-end/src/component/forms/Checkbox.jsx
@@ -6,8 +6,8 @@ export const Checkbox = props => {
       <input type="checkbox" id={props.id} className="activity-input" value={props.value} checked={props.isChecked} onClick={props.handleCheck}/>
       <label className="activity-icon" for={props.id}>
         <img src={props.image}/> 
-        <span className="text">{props.value}</span>
       </label>
+      <p className="text">{props.value}</p>
     </li>
   )
 }
