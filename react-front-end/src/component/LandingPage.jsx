4f53d593d5695c9bbcc50116b388b1a03f3308f2
Turner Schumann <turnerschumann@gmail.com>
2019-06-30 20:54:27 +0000

    Landing Page styling

--- react-front-end/src/component/LandingPage.jsx
+++ react-front-end/src/component/LandingPage.jsx
@@ -4,6 +4,7 @@ import leaftopleft from '../assets/leaf-top-left.png';
 import leaftopright from '../assets/leaf-top-right.png';
 import leafbottomleft from '../assets/leaf-bottom-left.png';
 import leafbottomright from '../assets/leaf-bottom-right.png';
+import "../styles/LandingPage.css"
 
 import { Link, Switch, Route } from 'react-router-dom';
 
@@ -53,13 +54,13 @@ class LandingPage extends Component {
         : null;
     return (
       <Layout>
-        <Header className="header" style={{background: 'white', paddingLeft: 0, borderBottom: '1px solid #c3cfd7' }}>
+        <Header className="header" style={{background: '#66615a', paddingLeft: '20px', fontSize: '200%', fontWeight: '800', color: 'white', height: '100px'}}>
           Reflect
         </Header>
  {/*       <div style={{ float: 'left' }}>
           <img src={landingscene} alt="LandingScene" style={{ float: 'left' }} />
         </div>*/}
-        <div style={{background: 'white', height: '100%'}}>
+        <div style={{background: '#f4f3ee', height: '100%'}}>
           <div style={{ float: 'left', margin: "0 0 0 0",width: '100px' }}>
             <img src={leaftopleft} alt="LeafTopLeft" style={{width: '100%', height: 'auto'}} />
           </div>
@@ -84,11 +85,11 @@ class LandingPage extends Component {
                 <Input placeholder="Password" type="password" style={{ width: '265px', margin: "0 25px 0 0px"}}/>
               </Form.Item>
               <Checkbox style={{ padding: "0 20px 0 18px"}}> Remember me </Checkbox>
-              <a className="login-form-forgot" href=""> Forgot password </a>
+              <a className="login-form-forgot" href="" style={{ color: '#42d0ce'}}> Forgot password </a>
               <Form.Item {...buttonItemLayout} style={{ width: "100px", margin: "20px 0 20px 0" }}>
                 <Button type="primary" onClick={(event) => {this.props.action(event); }}>Submit</Button>
               </Form.Item>
-              <a href="" style={{ float: 'left', margin: "0 0 0 20px"}}>Or register now!</a>
+              <a href="" style={{ float: 'left', margin: "0 0 0 20px", color: '#42d0ce'}}>Or register now!</a>
             </Form>
           </Card>
         </div>
