72baedad1a5f670b8c555390341ad296034f14c4
Nelson Yu <nelson.tm.yu@gmail.com>
2019-07-01 10:12:02 -0700

    Added activities effects on hover and checked on activities page

--- react-front-end/src/component/forms/Checkbox.jsx
+++ react-front-end/src/component/forms/Checkbox.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 export const Checkbox = props => {
+  const checked = () => {
+    if (props.isChecked) {
+      return <img src={props.image2}/>
+    } else {
+      return <img src={props.image}/>
+    }
+  }
+
   return (
     <li className="activity">
       <input type="checkbox" id={props.id} className="activity-input" value={props.value} checked={props.isChecked} onClick={props.handleCheck}/>
       <label className="activity-icon" for={props.id}>
-        <img src={props.image}/> 
+        {checked()} 
       </label>
       <p className="text">{props.value}</p>
     </li>
